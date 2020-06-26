bf7900e56adc56d632804a80465cba93df7e5d5c
Олександр Гнідін <yozhegsan@gmail.com>
2020-06-26 16:58:07 +0300

    Туче

--- SocketApp3/Form1.Designer.cs
+++ SocketApp3/Form1.Designer.cs
@@ -324,7 +324,7 @@
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Налаштування датчиків";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
